5849f66f96936b4704a1670998238738f3c69b24
Stef Boone <stefboone@gmail.com>
2016-09-04 15:29:30 -0400

    included clickable button

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const boards =
     [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
     [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,10 +23,46 @@ for (const cellNode of cellNodes) {
     const y = coordArray[1];
     const boardVal = boards.board1[x][y];
     if (boardVal === 1) {
-      cellClicked.style.backgroundColor = 'red';
+      cellClicked.style.backgroundColor = 'maroon';
     } else {
-      cellClicked.style.backgroundColor = 'blue';
+      cellClicked.style.backgroundColor = 'lightblue';
     }
   });
 }
-console.log('arg matey!');
+// onclick open new window containing game rules
+
+// onclick restart new game
+// const newGame = newGame;
+// const restartPlay = restartPlay;
+// $('#restart-btn').click(function (e) {
+//     newGame();
+//     restartplay();
+// });
+
+// Open Modal
+
+// Get the modal
+const modal = document.getElementById('popUp');
+
+// Get the button that opens the modal
+const btn = document.getElementById('rules');
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function (open) {
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function (close) {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
